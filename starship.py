b8a4ef7fa0922e46d493f6528f2596c6c171abd9
Djinn01 <67978015+Djinn01@users.noreply.github.com>
2024-08-12 12:20:32 -0400

    Update starship.py

--- starship.py
+++ starship.py
@@ -1,6 +1,6 @@
 class Starship:
     starship_list = []
-
+#definir la clase
     def __init__(self, name, model, manufacturer, cost_in_credits, length, max_atmosphering_speed, crew, passengers, cargo_capacity, consumables, hyperdrive_rating, MGLT, starship_class, pilots, url):
         self.name = name
         self.model = model
@@ -18,6 +18,29 @@ class Starship:
         self.pilots = pilots  # Lista de URLs
         self.url = url
         Starship.starship_list.append(self)
-
+#funciones para cambiar de objetos a diccionarios y vice versa para poder subir y cargar los datos del txt
     def __str__(self):
         return f"Name: {self.name}, Model: {self.model}, Manufacturer: {self.manufacturer}"
+
+    def to_dict(self):
+        return {"name":self.name, 
+                "model":self.model,
+                "manufacturer": self.manufacturer
+            }   
+    def from_dict(cls, dict_data):
+        return cls(name=dict_data["name"], 
+                    model=dict_data["model"], 
+                    manufacturer=dict_data["manufacturer"],
+                    cost_in_credits=None,
+                    length=None,
+                    max_atmosphering_speed=None,
+                    crew=None,
+                    passengers=None,
+                    cargo_capacity=None,
+                    consumables=None,
+                    hyperdrive_rating=None,
+                    MGLT=None,
+                    starship_class=None,
+                    pilots=None,
+                    url=None
+                    )
