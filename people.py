64c76eabc0f5cad0887d177d966a1fe13dfed13e
Djinn01 <67978015+Djinn01@users.noreply.github.com>
2024-08-12 23:53:58 -0400

    Update people.py

--- people.py
+++ people.py
@@ -15,27 +15,34 @@ class People:
         People.people_list.append(self)
 #funciones para cambiar de objetos a diccionarios y vice versa para poder subir y cargar los datos del txt
     def __str__(self):
-        return f"Name: {self.name}, Gender: {self.gender}, Birth Year: {self.birth_year}"
+        return f"name: {self.name}, height: {self.height}, mass: {self.mass}, hair color:{self.hair_color}, skin color:{self.skin_color}, eye color: {self.eye_color}, birth year: {self.birth_year}, gender: {self.gender}, homeworld: {self.homeworld}, url: {self.url}"
     def to_dict(self):
         crew = []
         for person in self:
-            crew.append({"name":person.name, 
+            crew.append({"name":person.name,
+                    "height":person.height,
+                    "mass":person.mass,
+                    "hair color":person.hair_color,
+                    "skin color":person.skin_color,
+                    "eye color":person.eye_color,
+                    "birth year":person.birth_year,
                     "gender":person.gender, 
-                    "birth year":person.birth_year 
+                    "homeworld":person.homeworld,
+                    "url":person.url
                     })
         return crew
     def from_dict(cls, dict_data):
         crew = []
         for person in dict_data:
             crew.append(cls(name=person["name"], 
-                        height=None,
-                        mass=None,
-                        hair_color=None,
-                        skin_color=None,
-                        eye_color=None,
-                        gender=person["gender"], 
+                        height=person["heigh"],
+                        mass=person["mass"],
+                        hair_color=person["hair color"],
+                        skin_color=person["skin color"],
+                        eye_color=person["eye color"],
                         birth_year=person["birth year"],
-                        homeworld=None,
-                        url=None
+                        gender=person["gender"], 
+                        homeworld=person["homeworld"],
+                        url=person["url"]
                         ))
         return crew
