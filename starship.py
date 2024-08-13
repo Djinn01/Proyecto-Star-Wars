7ab57bdd1b6b0b171349c7563c396874ea511eef
Djinn01 <67978015+Djinn01@users.noreply.github.com>
2024-08-12 23:53:20 -0400

    Update starship.py

--- starship.py
+++ starship.py
@@ -20,27 +20,39 @@ class Starship:
         Starship.starship_list.append(self)
 #funciones para cambiar de objetos a diccionarios y vice versa para poder subir y cargar los datos del txt
     def __str__(self):
-        return f"Name: {self.name}, Model: {self.model}, Manufacturer: {self.manufacturer}"
+        return f"Name: {self.name}, Model: {self.model}, Manufacturer: {self.manufacturer}, Cost in credits: {self.cost_in_credits}, length: {self.length}, max atmosphering speed: {self.max_atmosphering_speed}, crew: {self.crew}, passengers: {self.passengers}, cargo capacity: {self.cargo_capacity}, consumables: {self.consumables}, hyperdrive rating:{self.hyperdrive_rating}, MGLT: {self.MGLT}, starship class: {self.starship_class}, pilots: {self.pilots}, url: {self.url}"
 
     def to_dict(self):
         return {"name":self.name, 
                 "model":self.model,
-                "manufacturer": self.manufacturer
+                "manufacturer": self.manufacturer,
+                "Cost in credits": self.cost_in_credits,
+                "length": self.length,
+                "max atmosphering speed": self.max_atmosphering_speed,
+                "crew": self.crew,
+                "passengers": self.passengers,
+                "cargo capacity": self.cargo_capacity,
+                "consumables": self.consumables,
+                "hyperdrive rating": self.hyperdrive_rating,
+                "MGLT": self.MGLT,
+                "starship class": self.starship_class,
+                "pilots": self.pilots,
+                "url": self.url
             }   
     def from_dict(cls, dict_data):
         return cls(name=dict_data["name"], 
                     model=dict_data["model"], 
                     manufacturer=dict_data["manufacturer"],
-                    cost_in_credits=None,
-                    length=None,
-                    max_atmosphering_speed=None,
-                    crew=None,
-                    passengers=None,
-                    cargo_capacity=None,
-                    consumables=None,
-                    hyperdrive_rating=None,
-                    MGLT=None,
-                    starship_class=None,
-                    pilots=None,
-                    url=None
+                    cost_in_credits=dict_data["cost in credits"],
+                    length=dict_data["length"],
+                    max_atmosphering_speed=dict_data["max atmosphering speed"],
+                    crew=dict_data["crew"],
+                    passengers=dict_data["passengers"],
+                    cargo_capacity=dict_data["cargo capacity"],
+                    consumables=dict_data["consumables"],
+                    hyperdrive_rating=dict_data["hyperdrive rating"],
+                    MGLT=dict_data["MGLT"],
+                    starship_class=dict_data["starship class"],
+                    pilots=dict_data["pilots"],
+                    url=dict_data["url"],
                     )
